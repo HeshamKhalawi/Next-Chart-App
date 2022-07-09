01da1b29cc90f664bd257e2bc4e1e8da5cf8a943
hesham <heshamkhalawi@gmail.com>
2022-07-09 12:46:12 +0300

    added logic to slider

--- components/spendings/Card.js
+++ components/spendings/Card.js
@@ -2,19 +2,46 @@ import Spendings from "./Spendings";
 import Slider from "./Chart/Slider";
 import Total from './total/total'
 function Card(){
-    const cost = [{
-        "Mon": "33.52",
-        "Tue": "23.24",
-        "Wed": "54.52",
-        "Thu": "85.24",
-        "Fri": "48.32",
-        "Sat": "24.45",
-        "Sun": "56.22"
+    const cost = [
+        {
+        "Day": "Mon",
+        "Cost": "36.32"
+        },{
+        "Day": "Tue",
+        "Cost": "53.55"
+        },{
+        "Day": "Wed",
+        "Cost": "83.25"
+        },{
+        "Day": "Thu",
+        "Cost": "43.65"
+        },{
+        "Day": "Fri",
+        "Cost": "15.37"
+        },{
+        "Day": "Sat",
+        "Cost": "35.75"
+        },{
+        "Day": "Sun",
+        "Cost": "23.77"
     }]
+    
+    let mostExpensive = 0;
+    cost.map((Cost) => {
+        if(mostExpensive < Cost.Cost){
+            mostExpensive = Cost.Cost
+        }
+    })
+            
+        
     return(
         <div className="flex flex-col shadow-md w-[30rem] bg-white rounded-xl mt-4">
             <Spendings />
-            {cost.map()}
+            <div className="grid grid-cols-7 p-5 gap-7">
+                {cost.map((Cost) => 
+                    <Slider Cost={Cost} mostExpensive={mostExpensive}/>
+                )}
+            </div>
             <Total />
         </div>
     )
--- components/spendings/Chart/Slider.js
+++ components/spendings/Chart/Slider.js
@@ -1,8 +1,15 @@
-
-function Slider(){
-
+import { useState } from "react";
+function Slider({Cost, mostExpensive}){
+    const[hover, setHover] = useState(false);
+    let height = (Cost.Cost * 10)/mostExpensive;
+    console.log(height);
     return(
-        <div>
+        <div className="flex flex-col items-center">
+            <div className={hover ? "":`bg-[#EC775F] w-[3rem] rounded-lg ` + "h-[" + height + "rem]"}>
+
+            </div>
+            <div className="pt-2 font-sans text-[18px] font-normal mb-2 text-[#93867B]">{Cost.Day}</div>
+           
         </div>
     )
 }
